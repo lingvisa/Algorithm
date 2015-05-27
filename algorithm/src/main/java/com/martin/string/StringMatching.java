e412561eadc05637c08b515d71bdfc0363cd9653
congmin <lingvisa@gmail.com>
2015-05-27 03:51:31 -0400

    indexOf--- the Java version of strstr() submitted.

---
+++ algorithm/src/main/java/com/martin/string/StringMatching.java
@@ -0,0 +1,55 @@
+package com.martin.string;
+
+public class StringMatching {
+
+	/*
+	 *  Problem: Returns the index within the string 'str' of the first occurrence of the specified substring 'pattern'. 
+	 *  Example: indexOf("abcde", "de") == 3         
+	 * 
+	 * */
+	static int indexOf(String str, String pattern) {
+
+		// or throw InvalidArgumentException
+		if(str==null || pattern==null ) {
+			return -1;
+		}
+
+		if(pattern.length() > str.length()) {
+			return -1;
+		}
+
+		// max possible value of the index of the first match
+		int max = str.length() - pattern.length();		
+		for(int i=0; i<=max; i++) {
+			int j=0;
+			int k=i;
+			while(j<pattern.length()){
+				if(str.charAt(k) != pattern.charAt(j)) {
+					break;
+				}else{
+					j++;
+					k++;
+				}
+			}
+
+			// find the first match
+			if(j==pattern.length()) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		
+		assert indexOf("abcde", "de") == 3;
+		assert indexOf("abcde", "abc") == 0;
+		assert indexOf("abcde", "cd") == 2;
+		assert indexOf("abc", "abcd") == -1;
+		
+		System.out.println("Testing successful!");
+		
+	}
+
+}
