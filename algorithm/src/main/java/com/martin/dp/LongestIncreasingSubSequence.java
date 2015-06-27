5f90bee92c51885e718b66a078b5fbbc6b3d4bc1
Congmin Min <lingvisa@gmail.com>
2015-06-27 17:51:15 -0400

    DP of longest increasing subsequence length

--- algorithm/src/main/java/com/martin/dp/LongestIncreasingSubSequence.java
+++ algorithm/src/main/java/com/martin/dp/LongestIncreasingSubSequence.java
@@ -1,8 +1,10 @@
 package com.martin.dp;
 
-import java.util.ArrayList;
 
 /*
+ * Author: Congmnin Min
+ * Date: 06/26/2015
+ * 
  * Problem description:
 
  * The Longest Increasing Subsequence problem is to find the longest increasing subsequence of a given sequence. 
@@ -15,55 +17,66 @@ import java.util.ArrayList;
  * Implement both brute-force, and more efficient dynamic programming methods.
  * 
  * Algorithm:
+ *      Step 1: for each index 0<=i<n, find the largest length of subsequence L at index i, and store each L into an array
+ *              
+ *               
+ *      Step 2: Go through the array and find the largest length L' of the whole sequence
  *
  * */
 
 public class LongestIncreasingSubSequence {
 
-	/* First version, which doesn't work! */
-	static Object[] lis(int[] sequence) {
+	
+	/*
+	 * Input: sequence = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+	 * Output: 6
+	 * 
+	 * */
+	static int lisLength(int[] sequence) {
 		
 		int length = sequence.length;
+		int[] sub = new int[length];
 		
-		ArrayList<Integer> maxSequence = new ArrayList<Integer>();
-		for(int i=0; i<length-1; i++) {
-			int initial = sequence[i];
-			for(int j=i+1; j<length; j++){
-				ArrayList<Integer> subSequence = new ArrayList<Integer>();
-				subSequence.add(initial);
-				
-				int current = initial;
-				
-				int k = j;
-				while(k<length) {
-					int next = sequence[k];	
-					if(current<next){
-						subSequence.add(next);
-						current = next;
-					}
-					k++;
+		for(int i=0; i<length; i++) {
+			// Initialize the DP used first value
+			sub[i] = 1;
+			for(int j=0; j<i; j++) {
+				if(sequence[i]>sequence[j] && sub[i]<=sub[j] ){
+					// directly fetch value from the DP variable without re-caculation.
+					sub[i] = sub[j]+1;
 				}
-				if(maxSequence.size() < subSequence.size()){
-					maxSequence = subSequence;
-				}				
 			}
 		}
 		
-		return maxSequence.toArray();
+		// Find the largest element in the DP array
+		int largest = 0;
+		for(int i=0; i<length; i++){
+			if(largest<sub[i]) {
+				largest = sub[i];
+			}
+		}
+		
+		return largest;
 	}
 	
-	static void testLis() {
+	static void test() {
 		
 		int[] sequence = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
-		for(Object o : lis(sequence)) {
-			System.out.print(o + " ");
-		}
-		System.out.println(lis(sequence).toString());
+		int[] sequence1 = {0, 13, 3, 11, 7, 15};
+		int[] sequence2 = {10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+		int[] sequence3 = {0, 0, 11, 7, 15};
+		
+		assert lisLength(sequence)==6;
+		assert lisLength(sequence1)==4;
+		assert lisLength(sequence2)==4;
+		assert lisLength(sequence3)==3;
+		
+		System.out.println("test lisLength() successful!");
 	}
 	
 	public static void main(String[] args) {
 	
-		testLis();
+		test();
 
 	}
 
